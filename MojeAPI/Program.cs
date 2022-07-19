5f479ea3ecfcdcc3a20f31fdc7a6a97a7e04528a
Maciej Taszarek <maciej.taszarek@billennium.com>
2022-07-19 09:11:42 +0200

    Added Basic Swagger Authorization

--- MojeAPI/Program.cs
+++ MojeAPI/Program.cs
@@ -1,6 +1,7 @@
 using MojeAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using MojeAPI.Data.Services;
+using MojeAPI.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,15 +9,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-
+builder.Services.AddMvc(options =>
+{
+    options.SuppressAsyncSuffixInActionNames = false;
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// builder.Services.UseMiddleware<SwaggerBasicAuthMiddleware>();
+
 builder.Services.AddDbContext<LibraryContext>(
     option => option.UseSqlServer(builder.Configuration.GetConnectionString("BooksConnectionString"))
     );
+
+
 
 builder.Services.AddScoped<IBookService, BookService>();
 builder.Services.AddScoped<IFileService, FileService>();
@@ -26,15 +34,24 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
 }
+
+app.UseAuthorization();
+app.UseSwaggerAuthorized();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MojeAPI v1"));
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.MapControllers();
+
+// Using CORS to working API properly with Angular FRONT
+app.UseCors(x => x.AllowAnyHeader()
+      .AllowAnyMethod()
+      .AllowAnyOrigin()
+      .WithExposedHeaders("content-disposition")
+      );
 
 app.Run();
 
